0077adbf7d3f711eeb000910b5957f373acdca60
Aneruth Mohanasundaram <ane1998@gmail.com>
2021-06-13 15:51:12 +0200

    new funciton addition

--- Source Code/main.py
+++ Source Code/main.py
@@ -4,7 +4,6 @@ import numpy as np
 from skimage import io
 from matplotlib import pyplot as plt
 from skimage.util import random_noise
-from skimage.metrics import peak_signal_noise_ratio
 from imageio import imread
 
 ################################################### Part 1 ###################################################
@@ -13,20 +12,38 @@ from imageio import imread
 ################################################### ###### ###################################################
 
 # Loading the image
-img = imread('/Users/aneruthmohanasundaram/Documents/GitHub/Project_Alina/Data/Test2.png')
-
-# To perform image analysis
-print(f'Type of the given image is {type(img)}'+ '\n')
-print(f'Size of given image is {img.shape}' + '\n')
-print(f'Height of given image is {img.shape[0]}'+ '\n')
-print(f'Width of given image is {img.shape[1]}'+ '\n')
-print(f'Diemension of the given image is {img.ndim}')
+img = imread('/Users/aneruthmohanasundaram/Documents/GitHub/Project_Alina/Data/Test3.jpg')
 
 # A function to add random noise to our image
 def add_noise_to_image(image):
   ''' Considering only the gaussian noise filtering but we can choose different noise by changing the mode'''
   noise_image = random_noise(image,mode='gaussian',seed=None, clip=True)
   return noise_image
+
+# A function to check the Peak Signal Ratio
+def psnr(orgImg, nosiyImage): 
+	mse = np.mean((orgImg - nosiyImage) ** 2) 
+	if(mse == 0):
+		return 100
+	maximum_pixel_can_be_used = 255.0
+	ratio = 20 * log10(maximum_pixel_can_be_used / sqrt(mse)) 
+	return ratio
+
+def rgbToBlack(image):
+  image_copy = image
+  height, width, channels = image.shape
+  for i in range(height):
+    for j in range(width):
+        image_copy[i,j] = 0.3 * img[i,j][0] + 0.59 * img[i,j][1] +  0.11 * img[i,j][2]
+  return image_copy
+
+# To perform image analysis
+print(f'Type of the given image is {type(img)}'+ '\n')
+print(f'Size of given image is {img.shape}' + '\n')
+print(f'Height of given image is {img.shape[0]}'+ '\n')
+print(f'Width of given image is {img.shape[1]}'+ '\n')
+print(f'Diemension of the given image is {img.ndim}' + '\n')
+print(f'PSNR value is: {psnr(img, add_noise_to_image(img))} dB')
 
 fig,axes = plt.subplots(nrows=1,ncols=3,figsize=(15,8))
 
